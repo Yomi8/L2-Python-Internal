99e6d77f8934eaca0a84427b4c199576e53be01d
Yomi8 <127059233+Yomi8@users.noreply.github.com>
2024-03-18 20:54:18 +0000

    added shift function and changed user option selection

--- functions.py
+++ functions.py
@@ -9,26 +9,41 @@
 def encrypt(message, shift):
   output = []
   for character in message:
-    if character.isalpha():
-      if character.isupper():
-        output.append(chr((ord(character) + shift - 65) % 26 + 65))
-      else:
-        output.append(chr((ord(character) + shift - 97) % 26 + 97))
+    if character.isdigit():
+      output.append(str(int(character) + shift))
     else:
-      output.append(character)
+      if character.isalpha():
+        if character.isupper():
+          output.append(chr((ord(character) + shift - 65) % 26 + 65))
+        else:
+          output.append(chr((ord(character) + shift - 97) % 26 + 97))
+      else:
+        output.append(character)
   output = ''.join(output)
   return output
 
+
 def decrypt(message, shift):
   return encrypt(message, -shift)
+
 
 def file_write(file_name, message):
   with open(file_name, 'w') as file:
     file.write(message)
     file.close()
-    
+
+
 def file_read(file_name):
   with open(file_name, 'r') as file:
     message = file.readline()
     file.close()
   return message
+
+def get_shift(message):
+  while True:
+    try:
+        shift = int(input(message))
+        break
+    except ValueError:
+        print("Shift value must be an integer. Please try again.")
+  return shift
